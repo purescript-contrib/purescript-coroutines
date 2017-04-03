916a2ecb89717b73a21dbd5e4997d6a2629e453c
Phil Freeman <paf31@cantab.net>
2017-04-03 09:00:31 -0700

    0.11.1 updates in progress (#21)
    * 0.11.1 updates in progress
    
    * Fix tests

--- test/Main.purs
+++ test/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import Control.Coroutine as Co
 import Control.Coroutine (($$), ($~), (~$), (/\))
-import Control.Monad.Aff (Aff, later', launchAff)
+import Control.Monad.Aff (Aff, delay, launchAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
@@ -12,13 +12,14 @@ import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Rec.Class (forever)
 import Control.Monad.Trans.Class (lift)
 import Data.Maybe (Maybe(..))
+import Data.Newtype (wrap)
 
 nats :: forall eff. Co.Producer Int (Aff eff) Unit
 nats = go 0
   where
   go i = do
     Co.emit i
-    lift (later' 10 (pure unit)) -- 10ms delay
+    lift (delay (wrap 10.0)) -- 10ms delay
     go (i + 1)
 
 printer :: forall eff. Co.Consumer String (Aff (console :: CONSOLE | eff)) Unit
@@ -27,7 +28,7 @@ printer = forever do
   lift (liftEff (log s))
   pure Nothing
 
-showing :: forall a m. (Show a, Monad m) => Co.Transformer a String m Unit
+showing :: forall a m. Show a => Monad m => Co.Transformer a String m Unit
 showing = forever (Co.transform show)
 
 coshowing :: forall eff. Co.CoTransformer String Int (Aff (console :: CONSOLE | eff)) Unit
@@ -38,7 +39,7 @@ coshowing = go 0
     lift (liftEff (log o))
     go (i + 1)
 
-main :: forall eff. Eff (err :: EXCEPTION, console :: CONSOLE | eff) Unit
+main :: forall eff. Eff (exception :: EXCEPTION, console :: CONSOLE | eff) Unit
 main = void $ launchAff do
   Co.runProcess (showing `Co.fuseCoTransform` coshowing)
   Co.runProcess ((nats $~ showing) $$ printer)
